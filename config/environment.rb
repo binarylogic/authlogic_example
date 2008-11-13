c06e77d3590fba6218dd38139136f0d878d957fd
binarylogic <bjohnson@binarylogic.com>
2008-11-13 03:26:23 -0500

    Modified gem requirement

--- config/environment.rb
+++ config/environment.rb
@@ -25,7 +25,7 @@ Rails::Initializer.run do |config|
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem :authlogic
+  config.gem "authlogic"
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
