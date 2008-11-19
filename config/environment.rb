fa1cfd5c3aa3ab17c88e6eedfdf0032e0148aec4
binarylogic <bjohnson@binarylogic.com>
2008-11-19 15:20:07 -0500

    Removed local configuration

--- config/environment.rb
+++ config/environment.rb
@@ -25,14 +25,14 @@ Rails::Initializer.run do |config|
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "aws-s3", :lib => "aws/s3"
-  #config.gem "authlogic"
+  config.gem "authlogic"
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
-  config.plugin_paths += ["#{RAILS_ROOT}/../../Libs"]
-  config.plugins = [:authlogic]
+  #config.plugin_paths += ["#{RAILS_ROOT}/../../Libs"]
+  #config.plugins = [:authlogic]
 
   # Add additional load paths for your own custom dirs
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
@@ -68,12 +68,3 @@ Rails::Initializer.run do |config|
   # Activate observers that should always be running
   # config.active_record.observers = :cacher, :garbage_collector
 end
-
-ActionMailer::Base.smtp_settings = {
-  :address        => "smtp.binarylogic.com",
-  :port           => 25,
-  :domain         => "www.neco.com",
-  :user_name      => "neco@binarylogic.com",
-  :password       => "mizail29",
-  :authentication => :login
-}
