1d9d16dc5215185622fcb311fd577318851407ab
binarylogic <bjohnson@binarylogic.com>
2008-11-21 00:29:15 -0500

    Update tutorial links

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -1,5 +1,5 @@
 class Notifier < ActionMailer::Base
-  default_url_options[:host] = "authlogic_example.binarylogic.com"
+  default_url_options[:host] = "authlogicexample.binarylogic.com"
   
   def password_reset_instructions(user)
     subject       "Password Reset Instructions"
--- db/migrate/20081119233359_add_users_openid_field.rb
+++ db/migrate/20081119233359_add_users_openid_field.rb
@@ -10,8 +10,10 @@ class AddUsersOpenidField < ActiveRecord::Migration
 
   def self.down
     remove_column :users, :openid_identifier
-    change_column :users, :login, :string, :default => "", :null => false
-    change_column :users, :crypted_password, :string, :default => "", :null => false
-    change_column :users, :password_salt, :string, :default => "", :null => false
+    
+    [:login, :crypted_password, :password_salt].each do |field|
+      User.all(:conditions => "#{field} is NULL").each { |user| user.update_attribute(field, "") if user.send(field).nil? }
+      change_column :users, field, :string, :default => "", :null => false
+    end
   end
 end
