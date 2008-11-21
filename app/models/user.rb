847ac88718729bdcfcea709d74fdcff619922f5f
binarylogic <bjohnson@binarylogic.com>
2008-11-21 03:34:40 -0500

    Validate presence of password

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   acts_as_authentic :allow_blank_login_and_password_fields => true
   
   validates_presence_of :login, :if => Proc.new { |user| user.openid_identifier.blank? }
+  validates_presence_of :password, :if => Proc.new { |user| user.openid_identifier.blank? }
   validate :normalize_openid_identifier
   validates_uniqueness_of :openid_identifier, :allow_blank => true
   
