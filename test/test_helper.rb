ab30c1c5619bf422862f77ab27e72901ab0d514d
binarylogic <bjohnson@binarylogic.com>
2009-05-11 14:01:19 -0400

    Update tests to reflec the use of Authlogic::TestCase

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,9 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
 require 'test_help'
-require 'authlogic/testing/test_unit_helpers'
+require 'authlogic/test_case'
 
-class Test::Unit::TestCase
+class ActiveSupport::TestCase
   # Transactional fixtures accelerate your tests by wrapping each test method
   # in a transaction that's rolled back on completion.  This ensures that the
   # test database remains unchanged so your fixtures don't have to be reloaded
@@ -34,4 +34,8 @@ class Test::Unit::TestCase
   # Note: You'll currently still have to declare fixtures explicitly in integration tests
   # -- they do not yet inherit this setting
   fixtures :all
+end
+
+class ActionController::TestCase
+  setup :activate_authlogic
 end
